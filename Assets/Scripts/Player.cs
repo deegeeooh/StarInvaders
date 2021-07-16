5e99caaa6eaa1ba4bd55002c31e3bd1125bf347c
deegeeooh <bakkerpepijn69@gmail.com>
2021-07-16 16:33:38 +0200

    Added custom sprite > MjB

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,7 @@ public class Player : MonoBehaviour
 {
 
     [SerializeField] float player_MovementSpeed = 10f;
-    [SerializeField] float movementRestrictionX;
+    [SerializeField] float movementRestrictionX;                                                                               // movement restrictions for player ship
     [SerializeField] float movementRestrictionY_Top;
     [SerializeField] float movementRestrictionY_Bottom;
 
@@ -26,8 +26,8 @@ public class Player : MonoBehaviour
         Camera gameCamera = Camera.main;
         xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + movementRestrictionX;                                 // ViewportToWorldPoint has max coordinates 0.0 to 1.0
         xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x + movementRestrictionX * -1;                            // Since we are interested in X, only the other axis can be left zero
-        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + movementRestrictionY_Bottom;                                 // ViewportToWorldPoint has max coordinates 0.0 to 1.0
-        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - movementRestrictionY_Top;                            // Since we are interested in X, only the other axis can be left zero
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + movementRestrictionY_Bottom;                          
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - movementRestrictionY_Top;                            
 
 
     }
@@ -40,12 +40,12 @@ public class Player : MonoBehaviour
 
     private void Move()
      {
-        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * player_MovementSpeed;
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * player_MovementSpeed;                                   // using Time.deltatime to make movement FPS independent
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * player_MovementSpeed;
-        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);                                               // Mathf.Clamp to limit range
         var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
                
-        transform.position = new Vector2(newXPos, newYPos);
+        transform.position = new Vector2(newXPos, newYPos);                                                                 // Update player movement every frame
 
 
 
