348117053b4cbef28e478ad55a43bf3996822ed7
deegeeooh <bakkerpepijn69@gmail.com>
2021-07-16 19:14:09 +0200

    Added Laser prefab to player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,37 +5,57 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    [SerializeField] float player_MovementSpeed = 10f;
+    [SerializeField] float player_MovementSpeed = 11f;
     [SerializeField] float movementRestrictionX;                                                                               // movement restrictions for player ship
     [SerializeField] float movementRestrictionY_Top;
     [SerializeField] float movementRestrictionY_Bottom;
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float projectileSpeed = 10f;
 
     float xMin;
     float xMax;
     float yMin;
     float yMax;
-
+        
     // Start is called before the first frame update
     void Start()
     {
         SetupMoveBoundaries();
+        StartCoroutine(Checktime());
     }
 
-    private void SetupMoveBoundaries()
+    
+   
+
+    IEnumerator Checktime()
+
     {
-        Camera gameCamera = Camera.main;
-        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + movementRestrictionX;                                 // ViewportToWorldPoint has max coordinates 0.0 to 1.0
-        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x + movementRestrictionX * -1;                            // Since we are interested in X, only the other axis can be left zero
-        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + movementRestrictionY_Bottom;                          
-        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - movementRestrictionY_Top;                            
-
-
+        Debug.Log("First msg");
+        yield return new WaitForSeconds(5);                                                                                 // happens in same frame, then game goes on, returns after 3 seconds
+        Debug.Log("Second msg");
     }
+
+    
+
+
 
     // Update is called once per frame
     void Update()
     {
         Move();
+        Fire();
+    }
+
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))                                                                                   // use GetButton for autofire
+        {
+            GameObject laser = Instantiate(
+                laserPrefab, 
+                transform.position, 
+                Quaternion.identity) as GameObject;                                                                         // IDK what the fuck Quaternion does, but here we instantiate the lasersprite prefab at the center of the player
+                laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+        }
     }
 
     private void Move()
@@ -50,5 +70,16 @@ public class Player : MonoBehaviour
 
 
     }
+     private void SetupMoveBoundaries()
+        {
+            Camera gameCamera = Camera.main;
+            xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + movementRestrictionX;                                 // ViewportToWorldPoint has max coordinates 0.0 to 1.0
+            xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x + movementRestrictionX * -1;                            // Since we are interested in X, only the other axis can be left zero
+            yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + movementRestrictionY_Bottom;                          
+            yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - movementRestrictionY_Top;                            
+
+
+        }
+
 }
 
