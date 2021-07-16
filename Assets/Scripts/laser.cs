c34fbcf108cbf1c3445ec23a80b4fe3498e45e91
deegeeooh <bakkerpepijn69@gmail.com>
2021-07-16 19:14:32 +0200

    Added Audiosource to laser

---
+++ Assets/Scripts/laser.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laser : MonoBehaviour
+{
+    [SerializeField] AudioClip lasersound;
+
+    AudioSource myAudiosource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAudiosource = GetComponent<AudioSource>();
+        myAudiosource.PlayOneShot(lasersound);
+        Destroy(gameObject,2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
